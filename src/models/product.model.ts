18752e70dc3c1cbf5a414b018c7b8f40eea60824
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-08 20:12:43 +0600

    product model added

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,10 +2,41 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 
 
 interface IProduct extends Document {
-
+    product_name: string;
+    product_description: string;
+    price: number;
+    discount?: number | null;
+    product_thambnail: string;
+    quantity: number;
 }
 
 
-const ProductSchema: Schema<IProduct> = new mongoose.Schema({}, { timestamps: true })
+const ProductSchema: Schema<IProduct> = new mongoose.Schema({
+    product_name: {
+        type: String,
+        required: [true, "Product name is required"]
+    },
+    product_description: {
+        type: String,
+        required: [true, "Product description is required"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"]
+    },
+    discount: {
+        type: Number,
+        default: null
+    },
+    product_thambnail: {
+        type: String,
+        required: [true, "Product thabnail is required"]
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Product Quantity is required"]
+    }
+
+}, { timestamps: true })
 
 const ProductModel: Model<IProduct> = mongoose.model("product", ProductSchema)
