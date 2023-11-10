e6c57b2f33d68db69427bd0a43532164173288df
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-10 06:20:31 +0600

    update product done

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -29,6 +29,31 @@ const createProduct = catchAsync(async (req: Request, res: Response, next: NextF
         return next(new ErrorHandler(error.message, httpStatus.BAD_REQUEST))
     }
 })
+// admin only 
+const updateProduct = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = req.params?.id as string
+        const productData = req.body as IProduct
+        const updatedProductData = {
+            product_name: productData.product_name,
+            product_description: productData.product_description,
+            price: productData.price,
+            discount: productData.discount || null,
+            product_image: productData.product_image,
+            quantity: productData.quantity,
+        }
+        await productModel.findByIdAndUpdate(id, {
+            $set: updatedProductData
+        }, { new: true })
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: "product updated successfully"
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, httpStatus.BAD_REQUEST))
+    }
+})
 const getProducts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const products = await productModel.find()
@@ -45,6 +70,7 @@ const getProducts = catchAsync(async (req: Request, res: Response, next: NextFun
 
 const productController = {
     createProduct,
+    updateProduct,
     getProducts
 }
 
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,6 +6,7 @@ import { USER_ROLE } from "../enum/userRole"
 const productRouter = express.Router()
 
 productRouter.post("/create-product", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), productController.createProduct)
+productRouter.put("/update-product/:id", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), productController.updateProduct)
 productRouter.get("/get-all-products", productController.getProducts)
 
 export default productRouter
