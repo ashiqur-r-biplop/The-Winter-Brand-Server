f87cce8fd0dc38fd329bab277e194d18fbabf7b5
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-12 16:48:20 +0600

    cors update

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 import httpStatus from "http-status"
 import NodeCache from "node-cache"
 import morgan from "morgan"
+import helmet from "helmet"
+import bodyParser from "body-parser"
 import config from "./config"
 import errorHandlerMiddleware from "./middleware/errorHandler.middleware"
 import productRouter from "./routes/product.routes"
@@ -20,10 +22,31 @@ export const nodeCache = new NodeCache()
 app.use(morgan("dev"))
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
+// app.use(cors({
+//     origin: config.origin
+// }))
+
+
 app.use(cors({
-    origin: config.origin
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Enable cookies and authorization headers
 }))
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 
 app.use("/api/v1", productRouter, userRouter, reviewRouter, orderRouter, marketingRouter, contactRouter, layoutRouter)
