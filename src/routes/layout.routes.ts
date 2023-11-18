79eb30e78c2ba02a9c0629565fac6d91190e86c1
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-18 08:45:10 +0600

    layout apies secure done

--- src/routes/layout.routes.ts
+++ src/routes/layout.routes.ts
@@ -1,13 +1,16 @@
 import express from "express"
 import layoutController from "../controllers/layout.controller"
+import { authorizeRoles, isAuthenticated } from "../middleware/auth.middleware"
+import { USER_ROLE } from "../enum/userRole"
 
 const layoutRouter = express.Router()
 
-layoutRouter.post("/create-layout", layoutController.createLayout)
-layoutRouter.put("/update-featured-image", layoutController.updateFeaturedImage)
-layoutRouter.put("/delete-multiple-images", layoutController.deleteMultipleFeaturedImages)
-layoutRouter.delete("/delete-layout", layoutController.deleteLayout)
 layoutRouter.get("/get-faqs", layoutController.getFaqs)
 layoutRouter.get("/get-featured-images", layoutController.getFeaturedImage)
+// admin only 
+layoutRouter.post("/create-layout", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), layoutController.createLayout)
+layoutRouter.put("/update-featured-image", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), layoutController.updateFeaturedImage)
+layoutRouter.put("/delete-multiple-images", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), layoutController.deleteMultipleFeaturedImages)
+layoutRouter.delete("/delete-layout", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), layoutController.deleteLayout)
 
 export default layoutRouter
