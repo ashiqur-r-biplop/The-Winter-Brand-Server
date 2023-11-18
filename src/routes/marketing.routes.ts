a1b6c422d17726f3c656c814afdfcd37388d5568
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-18 08:41:46 +0600

    marketing route secure done

--- src/routes/marketing.routes.ts
+++ src/routes/marketing.routes.ts
@@ -5,10 +5,8 @@ import { USER_ROLE } from '../enum/userRole'
 
 const marketingRouter = express.Router()
 
-// marketingRouter.get("/get-email-marketing-data", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), marketingController.getEmailMarketingData)
-// marketingRouter.get("/get-phone-marketing-data", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), marketingController.getPhoneMarketingData)
-
-marketingRouter.get("/get-email-marketing-data", marketingController.getEmailMarketingData)
-marketingRouter.get("/get-phone-marketing-data", marketingController.getPhoneMarketingData)
+// only admin 
+marketingRouter.get("/get-email-marketing-data", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), marketingController.getEmailMarketingData)
+marketingRouter.get("/get-phone-marketing-data", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), marketingController.getPhoneMarketingData)
 
 export default marketingRouter
