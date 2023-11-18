9203476ab1950729260304b842f09a6f4208c79c
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-18 08:30:17 +0600

    order apies secure done in backend

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -5,28 +5,18 @@ import { USER_ROLE } from "../enum/userRole"
 
 const orderRouter = express.Router()
 
-// orderRouter.post("/create-order", orderController.createOrder)
-// orderRouter.post("/payment", orderController.newPayment)
-// orderRouter.post("/subscribe", orderController.newSubscribe)
-// orderRouter.put("/update-order-status/:id", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.updateOrderStatus)
-// orderRouter.delete("/delete-order/:id", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.deleteOrder)
-// orderRouter.get("/get-orders", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.getOrders)
-
-
-
-
-orderRouter.post("/create-order", orderController.createOrder)
-orderRouter.post("/payment", orderController.newPayment)
-orderRouter.post("/subscribe", orderController.newSubscribe)
-orderRouter.post("/unsubscribe", orderController.unsubscribe)
-orderRouter.put("/update-order-status", orderController.updateOrderStatus)
-orderRouter.delete("/delete-order/:id", orderController.deleteOrder)
-orderRouter.get("/get-orders", orderController.getOrders)
-orderRouter.get("/search-orders/:query", orderController.searchOrders)
-orderRouter.get("/get-orders-by-email", orderController.getOrdersByEmail)
-orderRouter.get("/get-invoice/:id", orderController.getInvoiceById)
+orderRouter.post("/create-order", isAuthenticated, orderController.createOrder)
+orderRouter.post("/payment", isAuthenticated, orderController.newPayment)
+orderRouter.post("/subscribe", isAuthenticated, orderController.newSubscribe)
+orderRouter.post("/unsubscribe", isAuthenticated, orderController.unsubscribe)
+orderRouter.get("/get-orders-by-email", isAuthenticated, orderController.getOrdersByEmail)
+orderRouter.get("/get-invoice/:id", isAuthenticated, orderController.getInvoiceById)
 // only admin 
-orderRouter.get("/get-order/:id", orderController.getSingleOrder)
+orderRouter.delete("/delete-order/:id", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.deleteOrder)
+orderRouter.put("/update-order-status", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.updateOrderStatus)
+orderRouter.get("/search-orders/:query", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.searchOrders)
+orderRouter.get("/get-orders", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.getOrders)
+orderRouter.get("/get-order/:id", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), orderController.getSingleOrder)
 
 
 export default orderRouter
