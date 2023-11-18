05cf5ed26b49ed0bb4beee6c841d837b1c01db44
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-18 08:48:56 +0600

    cart api secure done

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import cartController from "../controllers/cart.controller"
+import { isAuthenticated } from "../middleware/auth.middleware"
 
 const cartRouter = express.Router()
-cartRouter.post("/create-cart", cartController.createCart)
-cartRouter.put("/update-cart-product-quantity", cartController.updateCartQuantity)
-cartRouter.get("/get-cart/:email", cartController.getCartByEmail)
-cartRouter.get("/get-cart-is-exist", cartController.getIsCartExistByEmail)
-cartRouter.delete("/delete-cart/:id", cartController.deleteCart)
+cartRouter.post("/create-cart", isAuthenticated, cartController.createCart)
+cartRouter.put("/update-cart-product-quantity", isAuthenticated, cartController.updateCartQuantity)
+cartRouter.get("/get-cart/:email", isAuthenticated, cartController.getCartByEmail)
+cartRouter.get("/get-cart-is-exist", isAuthenticated, cartController.getIsCartExistByEmail)
+cartRouter.delete("/delete-cart/:id", isAuthenticated, cartController.deleteCart)
 
 export default cartRouter
