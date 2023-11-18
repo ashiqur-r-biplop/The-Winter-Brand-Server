384aef9b4bfc25ff397c23c0ca08d61be5b3344e
AbtahiHasan <anikidpersonal@gmail.com>
2023-11-18 08:49:54 +0600

    analytics apies secure done

--- src/routes/analytics.routes.ts
+++ src/routes/analytics.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import analyticsController from "../controllers/analytics.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth.middleware";
+import { USER_ROLE } from "../enum/userRole";
 const analyticsRouter = express.Router()
 
-analyticsRouter.get("/get-total-data-count", analyticsController.getTotalCreatedData)
-analyticsRouter.get("/get-recent-orders-reviews", analyticsController.getRecentOrdersReviews)
+analyticsRouter.get("/get-total-data-count", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), analyticsController.getTotalCreatedData)
+analyticsRouter.get("/get-recent-orders-reviews", isAuthenticated, authorizeRoles(USER_ROLE.ADMIN), analyticsController.getRecentOrdersReviews)
 
 export default analyticsRouter
